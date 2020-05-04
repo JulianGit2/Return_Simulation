ff33489efe498d0472eea97744700917665b8252
JulianGit2 <julian_stanger@web.de>
2020-05-04 20:04:46 +0200

    Second Draft

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ tax_rate = 0.25
 years = 30
 investment = 1000
 rate = 450
-simulations = 500
+simulations = 10000
 final_returns = []
 
 # Imports the Data with Date, Returns and Inflation
@@ -18,7 +18,7 @@ data = pd.read_excel(r"D:\PyCharm\Return_Simulation\Data.xlsx")
 inflation = pd.DataFrame(data, columns=["Inflation"]) + 1
 
 # Calculates net returns (after tax returns)
-returns = pd.DataFrame(data, columns=["S&P_500"]) * (1 - tax_rate)
+returns = (pd.DataFrame(data, columns=["S&P_500"]) * (1 - tax_rate)) + 1
 
 # Calculates net returns after inflation
 returns["S&P_500"] = returns["S&P_500"] / inflation["Inflation"]
@@ -27,7 +27,7 @@ returns["S&P_500"] = returns["S&P_500"] / inflation["Inflation"]
 def calc_return(years, investment, rate):
 
     for period in range(years):
-        per_return = (1 + rd.choice(returns["S&P_500"]))
+        per_return = (rd.choice(returns["S&P_500"]))
         investment = investment * per_return + rate
 
     return(investment)
@@ -36,6 +36,12 @@ def calc_return(years, investment, rate):
 for simulation in range(simulations):
     final_returns.append(calc_return(years, investment, rate))
 
+total_investment = investment + years * rate
 final_returns = np.round(final_returns, 1)
-print(final_returns)
+quint_low = np.quantile(final_returns, 0.05)
+quint_high = np.quantile(final_returns, 0.95)
 
+
+print(quint_low)
+print(quint_high)
+print(total_investment)
